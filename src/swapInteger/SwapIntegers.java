b4d152096eeb182b65ce69dd45fa85ccbd61e822
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-06-02 22:55:47 +0800

    [EXER]: Swap two numbers using only two variables

---
+++ src/swapInteger/SwapIntegers.java
@@ -0,0 +1,16 @@
+package swapInteger;
+
+public class SwapIntegers {
+
+  public static void main(String[] args) {
+    int a = 2;
+    int b = 3;
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+  }
+}
