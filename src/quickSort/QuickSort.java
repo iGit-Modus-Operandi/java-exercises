949500326a9f20325dc618f7b0bb92a2ae2a1241
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-06-18 20:51:45 +0800

    [EXER]: continuation of quick sort algorithm

--- src/quickSort/QuickSort.java
+++ src/quickSort/QuickSort.java
@@ -25,7 +25,32 @@ public class QuickSort {
     quickSort(numbers, 0, numbers.length - 1);
   }
 
-  private static void quickSort(int[] numbers, int i, int i1) {
+  private static void quickSort(int[] numbers, int lowIndex, int highIndex) {
+    if (lowIndex >= highIndex){
+      return;
+    }
+
+    int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
+    int pivot = numbers[pivotIndex];
+    swap(numbers, pivotIndex, highIndex);
+
+    int leftPointer = partition(numbers, lowIndex, highIndex, pivot);
+
+    quickSort(numbers, lowIndex, leftPointer - 1);
+    quickSort(numbers, leftPointer + 1, highIndex);
+  }
+
+  private static int partition(int[] numbers, int lowIndex, int highIndex, int pivot) {
+    int leftPointer = lowIndex;
+    int rightPointer = highIndex - 1;
+
+    return leftPointer;
+  }
+
+  private static void swap(int[] numbers, int index1, int index2) {
+    int temp = numbers[index1];
+    numbers[index1] = numbers[index2];
+    numbers[index2] = temp;
   }
 
   private static void printArray(int[] numbers) {
