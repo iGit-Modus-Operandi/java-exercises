2f9c987a00fd7976dbbd23468459229886680273
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-06-20 22:32:45 +0800

    [EXER]: Update bubble sort algorithm

--- src/bubbleSort/BubbleSort.java
+++ src/bubbleSort/BubbleSort.java
@@ -22,6 +22,19 @@ public class BubbleSort {
 
   }
 
+  private static void bubbleSort(int[] numbers) {
+    for (int i = 0; i < numbers.length - 1; i++)
+      if (numbers[i] > numbers[i + 1]){
+        swap(numbers, numbers[i], numbers[i + 1]);
+      }
+  }
+
+  private static void swap(int[] numbers, int index1, int index2) {
+    int temp = numbers[index1];
+    numbers[index1] = numbers[index2];
+    numbers[index2] = temp;
+  }
+
   private static void printArray(int[] numbers) {
     for (int i = 0; i < numbers.length; i++){
       System.out.println(numbers[i]);
