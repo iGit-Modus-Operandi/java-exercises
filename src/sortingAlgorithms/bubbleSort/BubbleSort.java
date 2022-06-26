24cafa9fcc8164a09967277106c45c52834798d6
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-06-26 23:32:41 +0800

    [EXER]: Edit some lines on buble sort algorithm

--- src/sortingAlgorithms/bubbleSort/BubbleSort.java
+++ src/sortingAlgorithms/bubbleSort/BubbleSort.java
@@ -42,7 +42,7 @@ public class BubbleSort {
 
   private static void printArray(int[] numbers) {
     for (int i = 0; i < numbers.length; i++) {
-      System.out.println(numbers[i]);
+      System.out.print(numbers[i] + " ");
     }
   }
 }
