444c115817928b42a79f9f1355cc8d5d3acb29ae
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-06-29 21:47:25 +0800

    [EXER]: Code merge sort algorithm initial steps

--- src/sortingAlgorithms/mergeSort/MergeSort.java
+++ src/sortingAlgorithms/mergeSort/MergeSort.java
@@ -22,11 +22,29 @@ public class MergeSort {
   }
 
   private static void mergeSort(int[] numbers) {
+    int inputLength = numbers.length;
+
+    if (inputLength < 2){
+      return;
+    }
+
+    int midIndex = inputLength / 2;
+    int [] leftHalf = new int[midIndex];
+    int [] rightHalf = new int[inputLength - midIndex];
+
+    for (int i = 0; i < midIndex; i++){
+      leftHalf[i] = numbers[i];
+    }
+
+    for (int i = midIndex; i < inputLength; i++){
+      rightHalf[i - midIndex] = numbers[i];
+    }
+
   }
 
   private static void printArray(int[] numbers) {
     for (int i = 0; i < numbers.length; i++){
-      System.out.println(numbers[i] + " ");
+      System.out.print(numbers[i] + " ");
     }
   }
 }
