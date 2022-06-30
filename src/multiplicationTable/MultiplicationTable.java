7bba6db715f013b647a30ee5b75dec1d374a8886
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-06-30 23:55:06 +0800

    [EXER]: Initial coding of multiplication table exercise

---
+++ src/multiplicationTable/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package multiplicationTable;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+
+
+
+  }
+}
