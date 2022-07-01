4fb129228d445ba29ba79b892090f4cb8a346388
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-02 01:14:07 +0800

    [EXER]: Initial code for BMI calculator

---
+++ src/bmiCalculator/BMICalculator.java
@@ -0,0 +1,12 @@
+/*
+* Weight (kg) Height (m) ^2
+* Weight (lb) Height (in) ^2 * 703
+*/
+package bmiCalculator;
+
+public class BMICalculator {
+
+  public static void main(String[] args) {
+
+  }
+}
