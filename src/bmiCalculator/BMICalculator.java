49c54d98d132f9816ea540d14f1b7897798db4ad
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-03 23:13:54 +0800

    [EXER]: Initial code for BMI calculator

--- src/bmiCalculator/BMICalculator.java
+++ src/bmiCalculator/BMICalculator.java
@@ -1,12 +1,22 @@
 /*
-* Weight (kg) Height (m) ^2
-* Weight (lb) Height (in) ^2 * 703
-*/
+ * Weight (kg) Height (m) ^2
+ * Weight (lb) Height (in) ^2 * 703
+ */
 package bmiCalculator;
+
+import java.util.Scanner;
 
 public class BMICalculator {
 
   public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter your weight in pounds: ");
+    double weight = scanner.nextDouble();
+
+    System.out.print("Enter your height in inches: ");
+    double height = scanner.nextDouble();
 
   }
 }
