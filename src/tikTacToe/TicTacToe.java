c71b7c77fc7327bd178536a29f4abe32fe6fbf04
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-04 21:39:32 +0800

    [EXER]: Finalize code for Tic Tac Toe game

--- src/tikTacToe/TicTacToe.java
+++ src/tikTacToe/TicTacToe.java
@@ -80,31 +80,67 @@ public class TicTacToe {
   private static boolean isValidMove(char[][] board, String position) {
     switch (position) {
       case "1":
-        return (board [0][0] == ' ');
+        return (board[0][0] == ' ');
       case "2":
-        return (board [0][1] == ' ');
+        return (board[0][1] == ' ');
       case "3":
-        return (board [0][2] == ' ');
+        return (board[0][2] == ' ');
       case "4":
-        return (board [1][0] == ' ');
+        return (board[1][0] == ' ');
       case "5":
-        return (board [1][1] == ' ');
+        return (board[1][1] == ' ');
       case "6":
-        return (board [1][2] == ' ');
+        return (board[1][2] == ' ');
       case "7":
-        return (board [2][0] == ' ');
+        return (board[2][0] == ' ');
       case "8":
-        return (board [2][1] == ' ');
+        return (board[2][1] == ' ');
       case "9":
-        return (board [2][2] == ' ');
+        return (board[2][2] == ' ');
       default:
         return false;
     }
   }
 
   private static boolean isGameFinished(char[][] board) {
+    if (hasContestantWon(board, 'X')) {
+      printBoard(board);
+      System.out.println("Player wins!");
+      return true;
+    }
 
+    if (hasContestantWon(board, 'O')) {
+      printBoard(board);
+      System.out.println("Computer wins!");
+      return true;
+    }
+
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] == ' ') {
+          return false;
+        }
+      }
+    }
+    printBoard(board);
+    System.out.println("The game ended in a tie!");
     return true;
+  }
+
+  private static boolean hasContestantWon(char[][] board, char symbol) {
+    if ((board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol) ||
+        (board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol) ||
+        (board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol) ||
+
+        (board[0][0] == symbol && board[1][0] == symbol && board[2][0] == symbol) ||
+        (board[0][1] == symbol && board[1][1] == symbol && board[2][1] == symbol) ||
+        (board[0][2] == symbol && board[1][2] == symbol && board[2][2] == symbol) ||
+
+        (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) ||
+        (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol)) {
+      return true;
+    }
+    return false;
   }
 
   private static void playerTurn(char[][] board, Scanner scanner) {
