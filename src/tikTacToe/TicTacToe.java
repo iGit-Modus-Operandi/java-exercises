fdb7bd02a6e890723018fc2a2056fe1402b2d423
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-04 21:25:32 +0800

    [EXER]: Update code for Tic Tac Toe game

--- src/tikTacToe/TicTacToe.java
+++ src/tikTacToe/TicTacToe.java
@@ -13,18 +13,18 @@ public class TicTacToe {
         {' ', ' ', ' '},
         {' ', ' ', ' '}
     };
-    
+
     printBoard(board);
 
-    while (true){
+    while (true) {
       playerTurn(board, scanner);
-      if (isGameFinished(board)){
+      if (isGameFinished(board)) {
         break;
       }
       printBoard(board);
 
       computerTurn(board);
-      if (isGameFinished(board)){
+      if (isGameFinished(board)) {
         break;
       }
       printBoard(board);
@@ -35,9 +35,9 @@ public class TicTacToe {
   private static void computerTurn(char[][] board) {
     Random random = new Random();
     int computerMove;
-    while(true){
+    while (true) {
       computerMove = random.nextInt(9) + 1;
-      if (isValidMove(board, Integer.toString(computerMove))){
+      if (isValidMove(board, Integer.toString(computerMove))) {
         break;
       }
     }
@@ -45,18 +45,87 @@ public class TicTacToe {
     placeMove(board, Integer.toString(computerMove), '0');
   }
 
-  private static void placeMove(char[][] board, String toString, char c) {
+  private static void placeMove(char[][] board, String position, char symbol) {
+    switch (position) {
+      case "1":
+        board[0][0] = symbol;
+        break;
+      case "2":
+        board[0][1] = symbol;
+        break;
+      case "3":
+        board[0][2] = symbol;
+        break;
+      case "4":
+        board[1][0] = symbol;
+        break;
+      case "5":
+        board[1][1] = symbol;
+        break;
+      case "6":
+        board[1][2] = symbol;
+        break;
+      case "7":
+        board[2][0] = symbol;
+        break;
+      case "8":
+        board[2][1] = symbol;
+        break;
+      case "9":
+        board[2][2] = symbol;
+        break;
+    }
   }
 
-  private static boolean isValidMove(char[][] board, String toString) {
+  private static boolean isValidMove(char[][] board, String position) {
+    switch (position) {
+      case "1":
+        return (board [0][0] == ' ');
+      case "2":
+        return (board [0][1] == ' ');
+      case "3":
+        return (board [0][2] == ' ');
+      case "4":
+        return (board [1][0] == ' ');
+      case "5":
+        return (board [1][1] == ' ');
+      case "6":
+        return (board [1][2] == ' ');
+      case "7":
+        return (board [2][0] == ' ');
+      case "8":
+        return (board [2][1] == ' ');
+      case "9":
+        return (board [2][2] == ' ');
+      default:
+        return false;
+    }
   }
 
   private static boolean isGameFinished(char[][] board) {
+
+    return true;
   }
 
   private static void playerTurn(char[][] board, Scanner scanner) {
+    String userInput;
+    while (true) {
+      System.out.println("Where would you like to play? (1-9)");
+      userInput = scanner.nextLine();
+      if (isValidMove(board, userInput)) {
+        break;
+      } else {
+        System.out.println(userInput + " is not a valid move.");
+      }
+    }
+    placeMove(board, userInput, 'X');
   }
 
   private static void printBoard(char[][] board) {
+    System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+    System.out.println("-+-+-");
+    System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+    System.out.println("-+-+-");
+    System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
   }
 }
