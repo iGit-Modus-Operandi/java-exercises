29f0a46910806571101b47fa686af32b378b6458
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-06 22:28:27 +0800

    [EXER]: Add initial code for Sudoku game

---
+++ src/sudoku/Sudoku.java
@@ -0,0 +1,49 @@
+package sudoku;
+
+public class Sudoku {
+
+  private static final int GRID_SIZE = 9;
+
+  public static void main(String[] args) {
+    int[][] board = {
+        {7, 0, 2, 0, 5, 0, 6, 0, 0},
+        {0, 0, 0, 0, 0, 3, 0, 0, 0},
+        {1, 0, 0, 0, 0, 9, 5, 0, 0},
+        {8, 0, 0, 0, 0, 0, 0, 9, 0},
+        {0, 4, 3, 0, 0, 0, 7, 5, 0},
+        {0, 9, 0, 0, 0, 0, 0, 0, 8},
+        {0, 0, 9, 7, 0, 0, 0, 0, 5},
+        {0, 0, 0, 2, 0, 0, 0, 0, 0},
+        {0, 0, 7, 0, 4, 0, 2, 0, 3}
+    };
+
+    printBoard(board);
+
+    if (solvedBoard(board)) {
+      System.out.println("Solved successfully!");
+    } else {
+      System.out.println("Unsolvable board :(");
+    }
+
+    printBoard(board);
+  }
+
+  private static boolean solvedBoard(int[][] board) {
+    return true;
+  }
+
+  private static void printBoard(int[][] board) {
+    for (int row = 0; row < GRID_SIZE; row++){
+      if (row % 3 == 0 && row != 0){
+        System.out.println("-----------");
+      }
+      for (int column = 0; column < GRID_SIZE; column++){
+        if (column % 3 == 0 && column != 0){
+          System.out.println("|");
+        }
+        System.out.println(board [row][column]);
+      }
+      System.out.println();
+    }
+  }
+}
