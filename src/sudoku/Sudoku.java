fa0cf3e13306f994ff63104d07d9874eebd12205
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-07 23:28:57 +0800

    [EXER]: Update code for sudoku game

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -58,6 +58,17 @@ public class Sudoku {
   }
 
   private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+    int localBoxRow = row - row % 3;
+    int localBoxColumn = column - column % 3;
+
+    for (int i = localBoxRow; i < localBoxRow; i++){
+      for (int j = localBoxColumn; j < localBoxColumn + 3; j++){
+        if (board [i][j] == number){
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
   private static boolean isNumberInColumn(int[][] board, int number, int column) {
