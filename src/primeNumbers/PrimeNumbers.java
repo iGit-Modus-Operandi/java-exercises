9ca0e9d9aefac74d01750b6fd71bcf8bf403d047
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-10 18:43:16 +0800

    [EXER]: Add sample code for finding prime numbers up to a certain limit given by the user input

---
+++ src/primeNumbers/PrimeNumbers.java
@@ -0,0 +1,35 @@
+package primeNumbers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+  public static void main(String[] args) {
+    System.out.println("Enter the limit: ");
+
+    Scanner scanner = new Scanner(System.in);
+    int limit = scanner.nextInt();
+
+    List<Integer> primeNumbers = new ArrayList<>();
+
+    for (int numberCheck = 2; numberCheck <= limit; numberCheck++){
+      boolean isPrime = true;
+      for (int factor = 2; factor <= numberCheck / 2; factor++){
+        if (numberCheck % factor == 0){
+          isPrime = false;
+          break;
+        }
+      }
+
+      if (isPrime){
+        primeNumbers.add(numberCheck);
+      }
+    }
+    System.out.println("Prime numbers from 1 to " + limit + " are: ");
+    for (int number: primeNumbers){
+      System.out.println(number);
+    }
+  }
+}
