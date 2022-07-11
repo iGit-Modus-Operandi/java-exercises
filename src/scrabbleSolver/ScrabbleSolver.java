fb82f6c221093938f5cd13d1b36044a44c990f15
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-11 22:57:45 +0800

    [EXER]: Finalize code for scrabble solver

--- src/scrabbleSolver/ScrabbleSolver.java
+++ src/scrabbleSolver/ScrabbleSolver.java
@@ -1,18 +1,46 @@
 package scrabbleSolver;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class ScrabbleSolver {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Please enter the pool of letters: ");
     String letters = scanner.nextLine().toUpperCase();
 
     Map <Character, Integer> lettersCountMap = new getCharacterCountMap(letters);
 
+    BufferedReader reader = new BufferedReader(new FileReader("M:\\texts\\drafts\\academics\\auto-didact\\java\\exercises\\java-exercises\\src\\scrabbleSolver"));
+
+    System.out.println("All possible words you can make with those letters: ");
+
+    for (String currentWord = reader.readLine(); currentWord != null; currentWord = reader.readLine()){
+      Map <Character, Integer> currentWordMap = getCharacterCountMap(currentWord);
+
+      boolean canMakeCurrentWord = true;
+      for (Character character: currentWordMap.keySet()){
+        int currentWordCharCount = currentWordMap.get(character);
+        int letterCharCount = lettersCountMap.containsKey(character) ?
+            lettersCountMap.get(character) : 0;
+
+        if (currentWordCharCount > letterCharCount) {
+          canMakeCurrentWord = false;
+          break;
+        }
+      }
+      if (canMakeCurrentWord){
+        System.out.println(currentWord);
+      }
+    }
+
+    scanner.close();
+    reader.close();
   }
 
   private static Map <Character, Integer> getCharacterCountMap (String letters){
