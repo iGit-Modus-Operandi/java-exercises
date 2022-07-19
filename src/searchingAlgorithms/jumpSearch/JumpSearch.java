b132f28a8d70fb57b3d503fcd50d14da0288cced
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-19 23:37:40 +0800

    [EXER]: Add initial code for jump search algorithm

---
+++ src/searchingAlgorithms/jumpSearch/JumpSearch.java
@@ -0,0 +1,27 @@
+package searchingAlgorithms.jumpSearch;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JumpSearch {
+
+  public static void main(String[] args) {
+    Random random = new Random();
+    int [] numbers = new int[10];
+
+    for (int i = 0; i < numbers.length; i++) {
+      numbers [i] = random.nextInt(100);
+    }
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter the value to be searched: ");
+    int searchValue = scanner.nextInt();
+
+    int searchResult = jumpSearch (numbers, searchValue);
+
+
+  }
+
+  private static int jumpSearch(int[] numbers, int searchValue) {
+  }
+}
