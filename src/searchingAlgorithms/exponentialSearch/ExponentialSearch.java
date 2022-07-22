17525177d09b90bf63e67cd616268cad0e5b4069
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-22 20:54:41 +0800

    [EXER]: Add output statement after the searching algorithm was invoked

--- src/searchingAlgorithms/exponentialSearch/ExponentialSearch.java
+++ src/searchingAlgorithms/exponentialSearch/ExponentialSearch.java
@@ -8,7 +8,7 @@ public class ExponentialSearch {
 
   public static void main(String[] args) {
     Random random = new Random();
-    int [] numbers = new int[10];
+    int[] numbers = new int[10];
 
     for (int i = 0; i < numbers.length; i++) {
       numbers[i] = random.nextInt(100);
@@ -17,16 +17,22 @@ public class ExponentialSearch {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Enter the value to be searched: ");
     int searchValue = scanner.nextInt();
-    
-    exponentialSearch(numbers, numbers.length, searchValue);
+
+    int searchResult = exponentialSearch(numbers, numbers.length, searchValue);
+
+    if (searchResult < 0) {
+      System.out.println("No such value was found.");
+    } else {
+      System.out.println("The value you are searching was found at index " + searchResult);
+    }
   }
 
   private static int exponentialSearch(int[] numbers, int length, int searchValue) {
-    if (numbers[0] == searchValue){
+    if (numbers[0] == searchValue) {
       return 0;
     }
     int i = 1;
-    while (i < numbers.length && numbers[i] <= searchValue){
+    while (i < numbers.length && numbers[i] <= searchValue) {
       i = i * 2;
     }
 
