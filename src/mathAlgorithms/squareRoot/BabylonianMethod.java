70e1aac0a5564d11187046284bd43af558d5db4b
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-07-31 22:25:04 +0800

    [EXER]: Find the square root of a user input using the Babylonian method

---
+++ src/mathAlgorithms/squareRoot/BabylonianMethod.java
@@ -0,0 +1,17 @@
+package mathAlgorithms.squareRoot;
+
+import java.util.Scanner;
+
+public class BabylonianMethod {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int userInput = scanner.nextInt();
+    
+    squareRoot(userInput);
+  }
+
+  private static void squareRoot(int userInput) {
+  }
+}
