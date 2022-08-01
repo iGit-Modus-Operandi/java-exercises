bedaa1d8976be11fb592928e4fd4aa2566bc06be
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-01 22:44:32 +0800

    [NOTES]: Add commentaries to the Babylonian method of finding the square root

--- src/mathAlgorithms/squareRoot/BabylonianMethod.java
+++ src/mathAlgorithms/squareRoot/BabylonianMethod.java
@@ -1,3 +1,15 @@
+/*
+ * CONDITION: Given an input from the user, userInput, find the square root of userInput
+ * TIME COMPLEXITY: O(n^(1/2))
+ * AUXILIARY SPACE: O(1)
+ * SOLUTION:
+ * 1. Start with an arbitrary positive start value x.
+ * 2. Initialize y = 1.
+ * 3. Get the next approximation for root using average of x and y
+ * 4. Set y = userInput/x
+ * 5. Repeat step 3 and 4 until desired approximation is achieved.
+ */
+
 package mathAlgorithms.squareRoot;
 
 import java.util.Scanner;
