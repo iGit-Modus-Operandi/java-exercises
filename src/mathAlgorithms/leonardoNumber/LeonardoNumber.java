a9183ffd82fa0072c3359841779b136189dd276f
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-12 23:08:59 +0800

    [EXER]: Add code for finding the Leonardo numbers given user input

---
+++ src/mathAlgorithms/leonardoNumber/LeonardoNumber.java
@@ -0,0 +1,23 @@
+package mathAlgorithms.leonardoNumber;
+
+import java.util.Scanner;
+
+public class LeonardoNumber {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int userInput = scanner.nextInt();
+
+    int result = leonardoNumber(userInput);
+
+    System.out.println(result);
+  }
+
+  private static int leonardoNumber(int userInput) {
+    if (userInput == 0 || userInput == 1){
+      return 1;
+    }
+    return ((leonardoNumber(userInput - 1) + leonardoNumber(userInput - 2)) + 1);
+  }
+
+}
