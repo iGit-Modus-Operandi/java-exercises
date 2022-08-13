76d113bb0e47228ed975c023c5a5d4948ec3f044
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-13 22:19:35 +0800

    [EXER]: Update code for shell sort algorithm

--- src/sortingAlgorithms/shellSort/ShellSort.java
+++ src/sortingAlgorithms/shellSort/ShellSort.java
@@ -15,8 +15,24 @@ public class ShellSort {
     System.out.println("Before sorting");
     printArray(numbers);
 
+    shellSort(numbers);
+
     System.out.println("After sorting");
     printArray(numbers);
+  }
+
+  private static int shellSort(int[] numbers) {
+    for (int gap = numbers.length / 2; gap > 0; gap /= 2) {
+      for (int i = gap; i < numbers.length; i += 1) {
+        int temp = numbers[i];
+        int j;
+        for (j = i; j >= gap && numbers[j - gap] > temp; j -= gap) {
+          numbers[j] = numbers[j - gap];
+        }
+        numbers[j] = temp;
+      }
+    }
+    return 0;
   }
 
   private static void printArray(int[] numbers) {
