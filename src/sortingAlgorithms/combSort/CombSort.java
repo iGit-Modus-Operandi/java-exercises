1c36253679ca728c08a8ee5fbedc16d103afac37
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-15 21:10:16 +0800

    [EXER]:Add utility method for printing array elements

--- src/sortingAlgorithms/combSort/CombSort.java
+++ src/sortingAlgorithms/combSort/CombSort.java
@@ -11,5 +11,17 @@ public class CombSort {
     for (int i = 0; i < numbers.length; i++) {
       numbers[i] = random.nextInt(100);
     }
+
+    System.out.println("Before sorting");
+    printArray(numbers);
+
+    System.out.println("After sorting");
+    printArray(numbers);
+  }
+
+  private static void printArray(int[] numbers) {
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.print(numbers[i] + " ");
+    }
   }
 }
