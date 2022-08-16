748408c7901cc68831eba9b9cf2ab8190dc0fc96
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-16 16:35:49 +0800

    [EXER]: Update code for comb sort algorithm

--- src/sortingAlgorithms/combSort/CombSort.java
+++ src/sortingAlgorithms/combSort/CombSort.java
@@ -15,8 +15,38 @@ public class CombSort {
     System.out.println("Before sorting");
     printArray(numbers);
 
+    combSort(numbers);
+
     System.out.println("After sorting");
     printArray(numbers);
+  }
+
+  private static void combSort(int[] numbers) {
+    int gap = numbers.length;
+    boolean swapped = true;
+
+    while (gap != 1 || swapped == true){
+      gap = getNextGap(gap);
+      swapped = false;
+
+      for (int i = 0; i < numbers.length - gap; i++) {
+        if (numbers[i] > numbers[i + gap]){
+          int temp = numbers[i];
+          numbers[i] = numbers[i + gap];
+          numbers[i + gap] = temp;
+
+          swapped = true;
+        }
+      }
+    }
+  }
+
+  private static int getNextGap(int gap) {
+    gap = (gap * 10) / 13;
+    if (gap < 1){
+      return 1;
+    }
+    return gap;
   }
 
   private static void printArray(int[] numbers) {
