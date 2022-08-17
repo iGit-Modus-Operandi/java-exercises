89bff49221d3703a4d4ebd3e94f86b03d0e38d15
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-17 22:52:24 +0800

    [EXER]: Add initial code for cycle sort algorithm

---
+++ src/sortingAlgorithms/cycleSort/CycleSort.java
@@ -0,0 +1,27 @@
+package sortingAlgorithms.cycleSort;
+
+import java.util.Random;
+
+public class CycleSort {
+
+  public static void main(String[] args) {
+    Random random = new Random();
+    int[] numbers = new int[10];
+
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = random.nextInt(100);
+    }
+
+    System.out.println("Before sorting");
+    printArray(numbers);
+
+    System.out.println("After sorting");
+    printArray(numbers);
+  }
+
+  private static void printArray(int[] numbers) {
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.print(numbers[i] + " ");
+    }
+  }
+}
