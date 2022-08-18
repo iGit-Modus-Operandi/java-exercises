678515aedcbda7ddd3668fb4fa57913b8d957546
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-18 22:40:27 +0800

    [EXER]: Update code fof the cycle sort algorithm

--- src/sortingAlgorithms/cycleSort/CycleSort.java
+++ src/sortingAlgorithms/cycleSort/CycleSort.java
@@ -15,8 +15,61 @@ public class CycleSort {
     System.out.println("Before sorting");
     printArray(numbers);
 
+    cycleSort(numbers);
+
     System.out.println("After sorting");
     printArray(numbers);
+  }
+
+  private static void cycleSort(int[] numbers) {
+    int writes = 0;
+
+    for (int cycleStart = 0; cycleStart < numbers.length; cycleStart++) {
+      int item = numbers[cycleStart];
+      int pos = cycleStart;
+
+      for (int i = cycleStart + 1; i < numbers.length; i++) {
+        if (numbers[i] < item){
+          pos++;
+        }
+      }
+
+      if (pos == cycleStart){
+        continue;
+      }
+
+      while (item == numbers[pos]){
+        pos += 1;
+      }
+
+      if (pos != cycleStart){
+        int temp = item;
+        item = numbers[pos];
+        numbers[pos] = temp;
+        writes++;
+      }
+
+      while (pos != cycleStart){
+        pos = cycleStart;
+
+        for (int i = cycleStart + 1; i < numbers.length; i++) {
+          if (numbers[i] < item){
+            pos += 1;
+          }
+        }
+
+        while (item == numbers[pos]){
+          pos += 1;
+        }
+
+        if (item != numbers[pos]){
+          int temp = item;
+          item = numbers[pos];
+          numbers[pos] = temp;
+          writes++;
+        }
+      }
+    }
   }
 
   private static void printArray(int[] numbers) {
