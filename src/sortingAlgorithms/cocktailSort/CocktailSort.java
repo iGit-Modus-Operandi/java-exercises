dd833d97a00889e187c47b96330d0fb48539a038
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-19 21:34:30 +0800

    [EXER]: Add initial code for cocktail sort algorithm

---
+++ src/sortingAlgorithms/cocktailSort/CocktailSort.java
@@ -0,0 +1,24 @@
+package sortingAlgorithms.cocktailSort;
+
+import java.util.Random;
+
+public class CocktailSort {
+
+  public static void main(String[] args) {
+    Random random = new Random();
+    int[] numbers = new int[10];
+
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = random.nextInt(100);
+    }
+
+    System.out.println("Before sorting");
+    printArray(numbers);
+    
+    System.out.println("After sorting");
+    printArray(numbers);
+  }
+
+  private static void printArray(int[] numbers) {
+  }
+}
