b33856c8b0221316c3e58b45288323b2bcd685b7
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-20 19:10:15 +0800

    [EXER]: Add utility function for printing arrays

--- src/sortingAlgorithms/cocktailSort/CocktailSort.java
+++ src/sortingAlgorithms/cocktailSort/CocktailSort.java
@@ -20,5 +20,8 @@ public class CocktailSort {
   }
 
   private static void printArray(int[] numbers) {
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.print(numbers[i] + " ");
+    }
   }
 }
