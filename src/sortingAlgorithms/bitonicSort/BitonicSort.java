1a7d841eacc0c1856efbeeb08122f5cc6f6bee37
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-23 17:35:50 +0800

    [EXER]: Update code for bitonic sort algorithm

--- src/sortingAlgorithms/bitonicSort/BitonicSort.java
+++ src/sortingAlgorithms/bitonicSort/BitonicSort.java
@@ -12,11 +12,30 @@ public class BitonicSort {
       numbers[i] = random.nextInt(100);
     }
 
+    int up = 1;
+    
     System.out.println("Before sorting");
     printArray(numbers);
 
+    bitonicSort(numbers, 0, numbers.length, up);
+
     System.out.println("After sorting");
     printArray(numbers);
+  }
+
+  private static void bitonicSort(int[] numbers, int low, int count, int dir) {
+    if (count > 1){
+      int k = count / 2;
+      
+      bitonicSort(numbers, low, k, 1);
+
+      bitonicSort(numbers, low + k, k, 0);
+      
+      bitonicMerge(numbers, low, count, dir);
+    }
+  }
+
+  private static void bitonicMerge(int[] numbers, int low, int count, int dir) {
   }
 
   private static void printArray(int[] numbers) {
