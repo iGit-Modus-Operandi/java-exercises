2a3fee29504111890b499316b65fe6fa5d99885e
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-26 15:19:51 +0800

    [EXER]:Update code for pancake sort algorithm

--- src/sortingAlgorithms/pancakeSort/PancakeSort.java
+++ src/sortingAlgorithms/pancakeSort/PancakeSort.java
@@ -15,8 +15,28 @@ public class PancakeSort {
     System.out.println("Before sorting");
     printArray(numbers);
 
+    pancakeSort(numbers);
+
     System.out.println("After sorting");
     printArray(numbers);
+  }
+
+  private static int pancakeSort(int[] numbers) {
+    for (int curr_size = numbers.length; curr_size > 1; --curr_size) {
+      int mi = findMax(numbers, curr_size);
+
+      if (mi != curr_size - 1){
+        flip (numbers, mi);
+        flip(numbers, curr_size - 1);
+      }
+    }
+    return 0;
+  }
+
+  private static int findMax(int[] numbers, int curr_size) {
+  }
+
+  private static void flip(int[] numbers, int mi) {
   }
 
   private static void printArray(int[] numbers) {
