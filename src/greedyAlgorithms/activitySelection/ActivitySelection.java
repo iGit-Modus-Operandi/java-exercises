cc78ffe9d8e0291c4bf0271ee56f0912b9b7d9f5
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-31 21:54:15 +0800

    [EXER]:Add initial code for activity selection algorithm

---
+++ src/greedyAlgorithms/activitySelection/ActivitySelection.java
@@ -0,0 +1,15 @@
+package greedyAlgorithms.activitySelection;
+
+public class ActivitySelection {
+
+  public static void main(String[] args) {
+    int[] startTime = {1, 3, 0, 5, 8, 5};
+    int[] finishTime = {2, 4, 6, 7, 9, 9};
+    int totalActivities = startTime.length;
+
+    printMaxActivities(startTime, finishTime, totalActivities)
+  }
+
+  private static void printMaxActivities(int[] startTime, int[] finishTime, int totalActivities) {
+  }
+}
