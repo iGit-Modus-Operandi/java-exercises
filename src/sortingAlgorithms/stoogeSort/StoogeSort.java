5e65d09b2ccc1852b1edce15edebca479953ed82
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-08-31 21:43:35 +0800

    [EXER]:Update code for stooge sort algorithm

--- src/sortingAlgorithms/stoogeSort/StoogeSort.java
+++ src/sortingAlgorithms/stoogeSort/StoogeSort.java
@@ -15,8 +15,30 @@ public class StoogeSort {
     System.out.println("Before sorting");
     printArray(numbers);
 
+    stoogeSort(numbers, 0, numbers.length);
+
     System.out.println("After sorting");
     printArray(numbers);
+  }
+
+  private static void stoogeSort(int[] numbers, int low, int high) {
+    if (low >= high){
+      return;
+    }
+
+    if (numbers[low] > numbers[high]){
+      int temp = numbers[low];
+      numbers[low] = numbers[high];
+      numbers[high] = temp;
+    }
+
+    if (high - low + 1 > 2){
+      int t = (high - low + 1) / 3;
+
+      stoogeSort(numbers, low, high - t);
+      stoogeSort(numbers, low + t, high);
+      stoogeSort(numbers, low, high - t);
+    }
   }
 
   private static void printArray(int[] numbers) {
