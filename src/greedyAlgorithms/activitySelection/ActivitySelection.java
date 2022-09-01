dc02317a166b44664b660acb3dcb23e79683bf54
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-01 23:08:46 +0800

    [EXER]:Update code for activity selection algorithm

--- src/greedyAlgorithms/activitySelection/ActivitySelection.java
+++ src/greedyAlgorithms/activitySelection/ActivitySelection.java
@@ -7,9 +7,21 @@ public class ActivitySelection {
     int[] finishTime = {2, 4, 6, 7, 9, 9};
     int totalActivities = startTime.length;
 
-    printMaxActivities(startTime, finishTime, totalActivities)
+    printMaxActivities(startTime, finishTime, totalActivities);
   }
 
   private static void printMaxActivities(int[] startTime, int[] finishTime, int totalActivities) {
+    int i, j;
+
+    System.out.println("Following activities are selected: \n");
+    i = 0;
+    System.out.println(i + " ");
+
+    for (int j = 1; j < startTime.length; j++) {
+      if (startTime[j] >= finishTime[i]){
+        System.out.println(j + " ");
+        i = j;
+      }
+    }
   }
 }
