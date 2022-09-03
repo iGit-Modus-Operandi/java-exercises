381622037a26c682e046945d1441bf60ce43c290
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-03 20:14:21 +0800

    [EXER]:Update code for Egyptian fraction algorithm

--- src/greedyAlgorithms/egyptianFraction/EgyptianFraction.java
+++ src/greedyAlgorithms/egyptianFraction/EgyptianFraction.java
@@ -9,4 +9,31 @@ public class EgyptianFraction {
     System.out.println("Egyptian fraction representation of " + numerator + "/" + denominator + " is \n");
     printEgyptianFraction(numerator, denominator);
   }
+
+  private static void printEgyptianFraction(int numerator, int denominator) {
+    if (denominator == 0 || numerator == 0){
+      return;
+    }
+
+    if (denominator % numerator == 0){
+      System.out.println("1/" + denominator/numerator);
+      return;
+    }
+
+    if (numerator % denominator == 0){
+      System.out.println(numerator/denominator);
+      return;
+    }
+
+    if (numerator > denominator){
+      System.out.println(numerator/denominator + " + ");
+      printEgyptianFraction(numerator % denominator, denominator);
+      return;
+    }
+
+    int n = denominator/numerator + 1;
+    System.out.println("1/" + n + " + ");
+
+    printEgyptianFraction(numerator * n - denominator, denominator * n);
+  }
 }
