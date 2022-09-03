93a203d502d5e3561716e8298f2ea03ae9c30c96
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-03 20:21:03 +0800

    [EXER]:Add initial code for Huffman Coding algorithm

---
+++ src/greedyAlgorithms/huffmanCoding/HuffmanCoding.java
@@ -0,0 +1,13 @@
+package greedyAlgorithms.huffmanCoding;
+
+import java.util.Scanner;
+
+public class HuffmanCoding {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = 6;
+    char[] charArray = {'a', 'b', 'c', 'd', 'e', 'f'};
+    int[] charFreq = {5, 9, 12, 13, 16, 45};
+  }
+}
