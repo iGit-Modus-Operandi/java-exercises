657e402044c51d2e515d944b3553856a5b532717
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-04 22:01:26 +0800

    [EXER]:Add utility classes

---
+++ src/greedyAlgorithms/huffmanCoding/MyComparator.java
@@ -0,0 +1,11 @@
+package greedyAlgorithms.huffmanCoding;
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<HuffmanNode> {
+
+  @Override
+  public int compare(HuffmanNode x, HuffmanNode y) {
+    return x.data - y.data;
+  }
+}
