decdaaeee2503899a53a970af509ebe3eaeca795
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-06 18:38:41 +0800

    [EXER]:Add utility function for printing code

--- src/greedyAlgorithms/huffmanCoding/HuffmanCoding.java
+++ src/greedyAlgorithms/huffmanCoding/HuffmanCoding.java
@@ -52,6 +52,12 @@ public class HuffmanCoding {
   }
 
   private static void printCode(HuffmanNode root, String s) {
+    if (root.left == null && root.right == null && Character.isLetter(root.c)){
+      System.out.println(root.c + ":" + s);
+      return;
+    }
+    printCode(root.left, s + "0");
+    printCode(root.right, s + "1");
   }
 }
 
