d7d705b14d0c006f093f9957fa29c82534f3053e
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-09 20:33:37 +0800

    [EXER]:Add initial code for finding nth Catalan number

---
+++ src/mathAlgorithms/catalanNumbers/CatalanNumbers.java
@@ -0,0 +1,16 @@
+package mathAlgorithms.catalanNumbers;
+
+public class CatalanNumbers {
+
+  public static void main(String[] args) {
+    CatalanNumbers catalanNumbers = new CatalanNumbers();
+    for (int i = 0; i < 10; i++) {
+      System.out.print(catalanNumbers.catalan(i) + " ");
+    }
+  }
+
+  private int catalan(int n) {
+    int result = 0;
+    return result;
+  }
+}
