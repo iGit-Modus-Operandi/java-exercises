324bf64ca75be7bf88b739e7f54e0a47a6f70a4c
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-12 19:37:10 +0800

    [EXER]:Update code for finding permutation coefficient

--- src/mathAlgorithms/permutationCoefficient/PermutationCoefficient.java
+++ src/mathAlgorithms/permutationCoefficient/PermutationCoefficient.java
@@ -9,6 +9,17 @@ public class PermutationCoefficient {
 
   private static Object permutationCoefficient(int n, int k) {
     int P[][] = new int[n + 2][k + 2];
+
+    for (int i = 0; i <= n; i++) {
+      for (int j = 0; j <= Math.min(i, k) ; j++) {
+        if (j == 0){
+          P[i][j] = 1;
+        } else {
+          P[i][j] = P[i - 1][j] + (j * P[i - 1][j - 1]);
+        }
+        P[i][j = 1] = 0;
+      }
+    }
     return P[n][k];
   }
 }
