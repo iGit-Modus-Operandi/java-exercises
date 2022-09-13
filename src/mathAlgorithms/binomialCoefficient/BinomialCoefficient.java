ff3bbdd3e7d23c7d4dab7c28ddf3a73715e7dbba
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-13 18:58:22 +0800

    [EXER]:Add initial code for finding binomial coefficient

---
+++ src/mathAlgorithms/binomialCoefficient/BinomialCoefficient.java
@@ -0,0 +1,13 @@
+package mathAlgorithms.binomialCoefficient;
+
+public class BinomialCoefficient {
+
+  public static void main(String[] args) {
+    int n = 5, k = 2;
+    System.out.println("Value of C(%d, %d) is %d ", n, k, binomialCoeff(n, k));
+  }
+
+  private static int binomialCoeff(int n, int k) {
+    return 1;
+  }
+}
