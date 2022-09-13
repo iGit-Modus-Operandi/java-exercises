f7fc5c307b8ba3e485f9c8f3b84c9f1ac16914b1
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-13 20:24:54 +0800

    [EXER]:Update code for finding binomial coefficient

--- src/mathAlgorithms/binomialCoefficient/BinomialCoefficient.java
+++ src/mathAlgorithms/binomialCoefficient/BinomialCoefficient.java
@@ -4,10 +4,17 @@ public class BinomialCoefficient {
 
   public static void main(String[] args) {
     int n = 5, k = 2;
-    System.out.println("Value of C(%d, %d) is %d ", n, k, binomialCoeff(n, k));
+    System.out.printf("Value of C(%d, %d) is %d", n, k, binomialCoeff(n, k));
   }
 
   private static int binomialCoeff(int n, int k) {
-    return 1;
+    if (k > n){
+      return 0;
+    }
+    if (k == 0 || k == n){
+      return 1;
+    }
+
+    return binomialCoeff(n - 1, k - 1) + binomialCoeff(n - 1, k);
   }
 }
