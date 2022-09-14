7daceba409d53f12284212c418fee271d42c2d33
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-14 23:24:15 +0800

    [NOTES]:Add explanations and elaboration of methods used

--- src/stringAlgorithms/stringCase/StringCase.java
+++ src/stringAlgorithms/stringCase/StringCase.java
@@ -1,3 +1,4 @@
+/*This validation algorithm checks whether a given string is in upper or lower case*/
 package stringAlgorithms.stringCase;
 
 public class StringCase {
@@ -9,9 +10,15 @@ public class StringCase {
     System.out.println(isLowerCase("hello"));
     System.out.println(isLowerCase("HELLO"));
 
+//  The all match method guarantees that every character in the string matches the required condition,
+//    hence, this will results to false
     System.out.println(isUpperCase("heLLo"));
     System.out.println(isLowerCase("heLLo"));
   }
+
+//  The following methods uses stream api and the method reference operator (::)
+//  which is used to call a method by referring to it with the help of its class directly
+//  Syntax: <Class name>::<method name>
 
   private static boolean isLowerCase(String s) {
     return s.chars().allMatch(Character::isUpperCase);
