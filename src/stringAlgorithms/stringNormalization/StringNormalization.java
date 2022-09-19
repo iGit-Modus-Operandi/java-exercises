af9287aa6c056757b32517dad0e0e2afff64c5b6
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-19 19:35:19 +0800

    [NOTES]:Add notes for explanations

--- src/stringAlgorithms/stringNormalization/StringNormalization.java
+++ src/stringAlgorithms/stringNormalization/StringNormalization.java
@@ -1,3 +1,6 @@
+/*This validation algorithm normalizes string input by converting it into a format more standard and useable
+afterwards. It also removes extra unnecessary whitespaces and delimiters to have strings-only inputs.*/
+
 package stringAlgorithms.stringNormalization;
 
 public class StringNormalization {
@@ -7,6 +10,7 @@ public class StringNormalization {
   }
 
   private static String normalizeString(String s) {
-    return s.toLowerCase().trim().replace(",", "")
+    return s.toLowerCase().trim().replace(",", "");
+    // trim method removes whitespaces from the input string
   }
 }
