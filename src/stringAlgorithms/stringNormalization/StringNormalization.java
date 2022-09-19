72eb6b4aa46e6ebaf94f156983befdcac158c762
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-19 19:31:24 +0800

    [EXER]:Add code for normalizing string inputs

---
+++ src/stringAlgorithms/stringNormalization/StringNormalization.java
@@ -0,0 +1,12 @@
+package stringAlgorithms.stringNormalization;
+
+public class StringNormalization {
+
+  public static void main(String[] args) {
+    System.out.println(normalizeString("   Hello There, BUDDY "));
+  }
+
+  private static String normalizeString(String s) {
+    return s.toLowerCase().trim().replace(",", "")
+  }
+}
