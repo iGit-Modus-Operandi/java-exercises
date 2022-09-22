7178abc33733ec56adea2f652fc686e9bc56569d
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-22 19:38:06 +0800

    [EXER]:Add intial code for coin change problem

---
+++ src/dynamicProgramming/coinChange/CoinChange.java
@@ -0,0 +1,9 @@
+package dynamicProgramming.coinChange;
+
+public class CoinChange {
+
+  public static void main(String[] args) {
+    int[] coins = {1, 2, 3};
+    System.out.println(count(coins, coins.length, sum));
+  }
+}
