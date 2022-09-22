bae317f708ea66a33b3e7d260b3f9cc64c6daa55
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-22 19:40:07 +0800

    [EXER]:Update code for coin change problem

--- src/dynamicProgramming/coinChange/CoinChange.java
+++ src/dynamicProgramming/coinChange/CoinChange.java
@@ -1,9 +1,20 @@
 package dynamicProgramming.coinChange;
 
+import java.util.Scanner;
+
 public class CoinChange {
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter amount to be changed into coins: ");
+    
+    int sum = scanner.nextInt();
     int[] coins = {1, 2, 3};
+    
     System.out.println(count(coins, coins.length, sum));
+  }
+
+  private static boolean count(int[] coins, int length, int sum) {
   }
 }
