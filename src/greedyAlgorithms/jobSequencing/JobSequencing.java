383ce82a4e59a4df070d4577d02bb933e1b2b8b9
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-09-27 21:15:55 +0800

    [EXER]:Add initial code for job sequencing problem

---
+++ src/greedyAlgorithms/jobSequencing/JobSequencing.java
@@ -0,0 +1,25 @@
+package greedyAlgorithms.jobSequencing;
+
+import java.util.ArrayList;
+
+public class JobSequencing {
+
+  char id;
+  int deadline;
+  int profit;
+
+  public Job(){
+  }
+
+  public Job(char id, int deadline, int profit) {
+    this.id = id;
+    this.deadline = deadline;
+    this.profit = profit;
+  }
+
+
+
+  public static void main(String[] args) {
+    ArrayList<Job> arr = new ArrayList<Job>();
+  }
+}
