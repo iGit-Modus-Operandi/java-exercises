b3e87873451d95949de3ce738b9c3df5cd5943c4
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-10-03 21:53:55 +0800

    [EXER]:Add first method for finding even numbers in the given arrays

--- src/arrayAlgorithms/arrayAggregation/ArrayAggregation.java
+++ src/arrayAlgorithms/arrayAggregation/ArrayAggregation.java
@@ -1,5 +1,6 @@
 package arrayAlgorithms.arrayAggregation;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
@@ -20,5 +21,29 @@ public class ArrayAggregation {
 
     Arrays.stream(findEvenNums1(numbers1, numbers2))
         .forEach(System.out::println);
+    System.out.println();
+    Arrays.stream(findEvenNums2(numbers1, numbers2))
+        .forEach(System.out::println);
+  }
+
+  private static int[] findEvenNums2(int[] numbers1, int[] numbers2) {
+    ArrayList<Integer> result = new ArrayList<>();
+
+    for (int num : numbers1) {
+      if (num % 2 == 0){
+        result.add(num);
+      }
+    }
+
+    for (int num : numbers2) {
+      if (num % 2 == 0){
+        result.add(num);
+      }
+    }
+
+    return result.stream().mapToInt(Integer::intValue).toArray();
+  }
+
+  private static int[] findEvenNums1(int[] numbers1, int[] numbers2) {
   }
 }
