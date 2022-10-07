37368577be0db9004f0d6264904c872003d5f5f0
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-10-07 23:02:24 +0800

    [NOTES]:Add commentaries on linked lists methods

--- src/linkedListAlgorithms/builtInMethods/BuiltInMethods.java
+++ src/linkedListAlgorithms/builtInMethods/BuiltInMethods.java
@@ -6,6 +6,7 @@ public class BuiltInMethods {
 
   public static void main(String[] args) {
     LinkedList<String> sins = new LinkedList<>();
+    // Adding elements on a list
     sins.add("Greed");
     sins.add("Envy");
     sins.add("Gluttony");
@@ -14,7 +15,10 @@ public class BuiltInMethods {
     sins.add("Sloth");
     sins.add("Anger");
 
+    //Displaying number of items within a list
     System.out.println(sins.size());
+
+    //Finding the existence of an element or value within a list
     System.out.println(sins.contains("Disgust"));
   }
 }
