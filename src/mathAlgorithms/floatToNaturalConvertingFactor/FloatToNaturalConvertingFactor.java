cb6d969162bc627805059cad58713c0b50cd32e7
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-10-28 20:47:41 +0800

    [EXER]:Add initial code for finding smallest factor to convert decimal to a natural number

---
+++ src/mathAlgorithms/floatToNaturalConvertingFactor/FloatToNaturalConvertingFactor.java
@@ -0,0 +1,15 @@
+package mathAlgorithms.floatToNaturalConvertingFactor;
+
+import java.util.Scanner;
+
+public class FloatToNaturalConvertingFactor {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String userInput = scanner.nextLine();
+    System.out.println(convertToNatural(userInput));
+  }
+
+  private static boolean convertToNatural(String userInput) {
+  }
+}
