5d8fc131f5fc28ee8e6028610e02c5e5a2de33a4
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-10-30 21:29:33 +0800

    [EXER]:Edit code for finding smallest factor to convert float to natural number

--- src/mathAlgorithms/floatToNaturalConvertingFactor/FloatToNaturalConvertingFactor.java
+++ src/mathAlgorithms/floatToNaturalConvertingFactor/FloatToNaturalConvertingFactor.java
@@ -10,6 +10,30 @@ public class FloatToNaturalConvertingFactor {
     System.out.println(convertToNatural(userInput));
   }
 
-  private static boolean convertToNatural(String userInput) {
+  private static int convertToNatural(String userInput) {
+    int countAfterDot = 0;
+    boolean dotSeen = false;
+    int num = 0;
+    for (int i = 0; i < userInput.length(); i++) {
+      if (userInput.charAt(i) != '.'){
+        num = num * 10 + (userInput.charAt(i) - '0');
+        if (dotSeen == true){
+          countAfterDot++;
+        } else {
+          dotSeen = true;
+        }
+      }
+    }
+    if (dotSeen == false){
+      return 1;
+    }
+
+    int dem = (int) Math.pow(10, countAfterDot);
+
+    return (dem / gcd (num, dem));
+  }
+
+  private static int gcd(int a, int b) {
+
   }
 }
