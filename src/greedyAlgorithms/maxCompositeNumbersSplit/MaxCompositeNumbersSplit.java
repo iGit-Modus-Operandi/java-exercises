81ef6a186f699af250159af78ccf45f76730d0c4
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-11-02 20:22:45 +0800

    [EXER]: Update code for finding max number of composite numbers to split a number

--- src/greedyAlgorithms/maxCompositeNumbersSplit/MaxCompositeNumbersSplit.java
+++ src/greedyAlgorithms/maxCompositeNumbersSplit/MaxCompositeNumbersSplit.java
@@ -11,6 +11,33 @@ public class MaxCompositeNumbersSplit {
   }
 
   private static int maxCompositeNumbers(int n) {
-    return n;
+    if (n < 4){
+      return -1;
+    }
+
+    int rem = n % 4;
+
+    if (rem == 0){
+      return n/4;
+    }
+
+    if (rem == 1){
+      if (n < 9){
+        return -1;
+      }
+      return (n - 9) / 4 + 1;
+    }
+
+    if (rem == 2){
+      return (n - 6) / 4 + 1;
+    }
+
+    if (rem == 3){
+      if (n < 15){
+        return -1;
+      }
+      return (n - 15) / 4 + 2;
+    }
+    return 0;
   }
 }
