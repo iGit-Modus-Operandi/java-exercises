2e6a5ba5de1b38410c594496df655c2fa4af77f7
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-11-06 22:18:22 +0800

    [EXER]: Edit code for solving the water connection problem

--- src/greedyAlgorithms/waterConnection/WaterConnection.java
+++ src/greedyAlgorithms/waterConnection/WaterConnection.java
@@ -1,12 +1,19 @@
 package greedyAlgorithms.waterConnection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WaterConnection {
 
   static int numberOfHouses, numberOfPipes;
   static int startingVertexOfPipes[] = new int[1100];
   static int endingVertexOfPipes[] = new int[1100];
   static int diameterOfPipes[] = new int[1100];
-  
+
+  static List<Integer> a = new ArrayList<>();
+  static List<Integer> b = new ArrayList<>();
+  static List<Integer> c = new ArrayList<>();
+
   public static void main(String[] args) {
     numberOfHouses = 9;
     numberOfPipes = 6;
@@ -20,5 +27,20 @@ public class WaterConnection {
   }
 
   private static void solve(int[][] arr) {
+    int i = 0;
+    while (i < numberOfPipes){
+      int q = arr[i][0];
+      int h = arr[i][1];
+      int t = arr[i][2];
+
+      startingVertexOfPipes[q] = h;
+      diameterOfPipes[q] = t;
+      endingVertexOfPipes[h] = q;
+      i++;
+    }
+
+    a = new ArrayList<Integer>();
+    b = new ArrayList<Integer>();
+    c = new ArrayList<Integer>();
   }
 }
