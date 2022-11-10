be27f7733ebd279c57370e660ae9a47a33ee71af
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-11-10 21:35:17 +0800

    [EXER]: Edit code for solving job sequencing problem

--- src/greedyAlgorithms/jobSequencing/JobSequencing.java
+++ src/greedyAlgorithms/jobSequencing/JobSequencing.java
@@ -17,6 +17,15 @@ public class JobSequencing {
   }
 
   public static void main(String[] args) {
-    ArrayList<JobSequencing> arr = new ArrayList<JobSequencing>();
+    ArrayList<JobSequencing> arr = new ArrayList<>();
+    arr.add(new JobSequencing('a', 2, 100));
+    arr.add(new JobSequencing('b', 1, 19));
+    arr.add(new JobSequencing('c', 2, 27));
+    arr.add(new JobSequencing('d', 1, 25));
+    arr.add(new JobSequencing('e', 3, 15));
+
+    System.out.println("Following is maximum profit sequence of jobs");
+
+    JobSequencing jobSequence = new JobSequencing();
   }
 }
