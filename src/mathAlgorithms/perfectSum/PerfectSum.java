c7de308ee00741ba7c034b02579a55d6277b960d
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-11-19 21:34:17 +0800

    [EXER]: Add code for finding perfect sum

---
+++ src/mathAlgorithms/perfectSum/PerfectSum.java
@@ -0,0 +1,13 @@
+package mathAlgorithms.perfectSum;
+
+public class PerfectSum {
+
+  public static void main(String[] args) {
+    int[] arr = {1, 2, 3, 4, 5};
+    int sum = 10;
+    printAllSubsets(arr, arr.length, sum);
+  }
+
+  private static void printAllSubsets(int[] arr, int length, int sum) {
+  }
+}
