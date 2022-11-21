10884c33ae2b77fa2fe088a70a9d988d5f4395b3
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-11-21 21:55:45 +0800

    [EXER]: Add codes for solving the perfect sum problem

--- src/mathAlgorithms/perfectSum/PerfectSum.java
+++ src/mathAlgorithms/perfectSum/PerfectSum.java
@@ -1,6 +1,7 @@
 package mathAlgorithms.perfectSum;
 
 public class PerfectSum {
+  static boolean[][] dp;
 
   public static void main(String[] args) {
     int[] arr = {1, 2, 3, 4, 5};
@@ -9,5 +10,28 @@ public class PerfectSum {
   }
 
   private static void printAllSubsets(int[] arr, int length, int sum) {
+    if (length == 0 || sum < 0){
+      return;
+    }
+
+    dp = new boolean[length][sum + 1];
+    for (int i = 0; i < length; ++i) {
+      dp[i][0] = true;
+    }
+
+    if (arr[0] <= sum){
+      dp[0][arr[0]] = true;
+    }
+
+    for (int i = 0; i < length; ++i) {
+      for (int j = 0; j < sum + 1; ++j) {
+        dp[i][j] = (arr[i] <= j) ? (dp[i - 1][j] || dp[i - 1][j - arr[i]]) : dp[i - 1][j];
+      }
+    }
+
+    if (dp[length - 1][sum] == false){
+      System.out.println("There are no subsets with" + " sum " + sum);
+      return;
+    }
   }
 }
