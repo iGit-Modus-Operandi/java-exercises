da898e1b4475cda4d43ec785dc77f614394b5f63
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-12-14 23:29:21 +0800

    [EXER]: Add initial code for the maximum path sum in a triangle problem

---
+++ src/dynamicProgramming/maxPathSum/MaxPathSum.java
@@ -0,0 +1,12 @@
+package dynamicProgramming.maxPathSum;
+
+public class MaxPathSum {
+
+  public static void main(String[] args) {
+    int triangle[][] = {{1, 0, 0}, {4, 8, 0}, {1, 5, 3}};
+    System.out.println(maxPathSum(triangle, 0, 0, 3, 3));
+  }
+
+  private static boolean maxPathSum(int[][] triangle, int i, int i1, int row, int column) {
+  }
+}
