01358f591773a525aa547a2c90e57e696c55bb51
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-12-15 22:23:20 +0800

    [EXER]: Update code on solving the maximum path sum in a triangle

--- src/dynamicProgramming/maxPathSum/MaxPathSum.java
+++ src/dynamicProgramming/maxPathSum/MaxPathSum.java
@@ -7,6 +7,13 @@ public class MaxPathSum {
     System.out.println(maxPathSum(triangle, 0, 0, 3, 3));
   }
 
-  private static boolean maxPathSum(int[][] triangle, int i, int i1, int row, int column) {
+  private static int maxPathSum(int[][] triangle, int i, int j, int row, int column) {
+    if (j == column){
+      return 0;
+    }
+    if (i == row - 1){
+      return triangle[i][j];
+    }
+    return triangle[i][j] + Math.max(maxPathSum(triangle, i + 1, j, row, column), maxPathSum(triangle, i + 1, j + 1, row, column));
   }
 }
