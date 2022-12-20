ce4d4c55e524123d19bc9cd0c6b2fd77bb610208
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-12-20 21:04:05 +0800

    [EXER]: Add initial code for finding maximum length of snake sequence

---
+++ src/dynamicProgramming/maxLengthSnakeSequence/MaxLengthSnakeSequence.java
@@ -0,0 +1,17 @@
+package dynamicProgramming.maxLengthSnakeSequence;
+
+public class MaxLengthSnakeSequence {
+
+  public static void main(String[] args) {
+    int[][] matrix = {
+        {9, 6, 5, 2},
+        {8, 7, 6, 5},
+        {7, 3, 1, 6},
+        {1, 1, 1, 7}
+    };
+    findSnakeSequence(matrix);
+  }
+
+  private static void findSnakeSequence(int[][] matrix) {
+  }
+}
