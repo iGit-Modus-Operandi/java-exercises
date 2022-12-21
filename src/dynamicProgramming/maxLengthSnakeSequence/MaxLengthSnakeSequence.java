91d3bdf47d56db1fc70e5214bc8671e46823bb48
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-12-21 23:42:24 +0800

    [EXER]: Add code for the max length of snake sequence problem

--- src/dynamicProgramming/maxLengthSnakeSequence/MaxLengthSnakeSequence.java
+++ src/dynamicProgramming/maxLengthSnakeSequence/MaxLengthSnakeSequence.java
@@ -2,6 +2,9 @@ package dynamicProgramming.maxLengthSnakeSequence;
 
 public class MaxLengthSnakeSequence {
 
+  static int M = 4;
+  static int N = 4;
+
   public static void main(String[] args) {
     int[][] matrix = {
         {9, 6, 5, 2},
@@ -13,5 +16,27 @@ public class MaxLengthSnakeSequence {
   }
 
   private static void findSnakeSequence(int[][] matrix) {
+    int[][] lookup = new int[M][N];
+    int max_len = 0;
+    int max_row = 0;
+    int max_col = 0;
+
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        if (i != 0 || j != 0){
+          if (i > 0 && Math.abs(matrix[i - 1][j] - matrix[i][j]) == 1){
+            lookup[i][j] = Math.max(lookup[i][j], lookup[i - 1][j] + 1);
+
+            if (max_len < lookup[i][j]){
+              max_len = lookup[i][j];
+              max_row = i;
+              max_col = j;
+            }
+          }
+
+          if (j > 0 && Math.abs())
+        }
+      }
+    }
   }
 }
