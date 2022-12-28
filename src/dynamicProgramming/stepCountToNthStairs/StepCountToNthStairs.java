15a7d2463b0c16e5dec1adf0a2892fb92eafd440
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2022-12-28 23:11:44 +0800

    [EXER]: Add initial code for finding step count to reach nth stairs

---
+++ src/dynamicProgramming/stepCountToNthStairs/StepCountToNthStairs.java
@@ -0,0 +1,16 @@
+package dynamicProgramming.stepCountToNthStairs;
+
+import java.util.Scanner;
+
+public class StepCountToNthStairs {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int steps = scanner.nextInt();
+
+    System.out.println(countSteps(steps));
+  }
+
+  private static void countSteps(int steps) {
+  }
+}
