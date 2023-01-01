7ffd6eb911e52b84e31100f482cf5d2ee42562ab
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-01-01 22:33:05 +0800

    [EXER]: Add code for assigning mice to holes problem

---
+++ src/greedyAlgorithms/miceToHoles/MiceToHoles.java
@@ -0,0 +1,20 @@
+package greedyAlgorithms.miceToHoles;
+
+import java.util.ArrayList;
+
+public class MiceToHoles {
+
+  public static void main(String[] args) {
+    ArrayList<Integer> mice = new ArrayList<>();
+    mice.add(4);
+    mice.add(-4);
+    mice.add(2);
+
+    ArrayList<Integer> holes = new ArrayList<>();
+    holes.add(4);
+    holes.add(0);
+    holes.add(5);
+
+    System.out.println("The last mouse gets into " + "the hole in time: ");
+  }
+}
