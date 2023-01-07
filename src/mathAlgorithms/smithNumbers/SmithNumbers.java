3266a151e6623e6183e4904573b95ef185f877db
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-01-07 23:25:23 +0800

    [EXER]: Add utility method for finding Smith numbers

--- src/mathAlgorithms/smithNumbers/SmithNumbers.java
+++ src/mathAlgorithms/smithNumbers/SmithNumbers.java
@@ -2,6 +2,8 @@ package mathAlgorithms.smithNumbers;
 
 public class SmithNumbers {
 
+  static int MAX = 10000;
+
   public static void main(String[] args) {
     sieveOfSundaram();
     System.out.println("Printing first few Smith numbers using isSmith()");
@@ -13,6 +15,13 @@ public class SmithNumbers {
   }
 
   private static void sieveOfSundaram() {
+    boolean[] marked = new boolean[MAX/2 + 100];
+
+    for (int i = 1; i <= (Math.sqrt(MAX) - 1) / 2; i++) {
+      for (int j = (i * (i + 1)) << 1; j <= MAX / 2 ; j = j + 2 * i + 1) {
+        marked[j] = true;
+      }
+    }
   }
 
   private static boolean isSmith(int i) {
