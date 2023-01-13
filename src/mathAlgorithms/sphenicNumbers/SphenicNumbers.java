390f21a6e4006f06b7066e46e8e4ea24bf8410bb
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-01-13 20:47:02 +0800

    [EXER]: Edit code for finding Sphenic numbers

--- src/mathAlgorithms/sphenicNumbers/SphenicNumbers.java
+++ src/mathAlgorithms/sphenicNumbers/SphenicNumbers.java
@@ -5,7 +5,7 @@ public class SphenicNumbers {
   public static void main(String[] args) {
     int n = 60;
     simpleSieve();
-    int ans = find_sphere(n);
+    int ans = find_sphene(n);
     if (ans == 1){
       System.out.println("Yes");
     }
@@ -17,6 +17,19 @@ public class SphenicNumbers {
   private static void simpleSieve() {
   }
 
-  private static int find_sphere(int n) {
+  private static int find_sphene(int n) {
+    int[] arr1 = new int[8];
+    int count = 0;
+    int j = 0;
+    for (int i = 1; i < n; i++) {
+      count++;
+      arr1[j++] = i;
+    }
+
+    //TODO: problematic code
+//    if (count == 8 && (arr1[arr1[1]] && arr1[arr1[2]] && arr1[arr1[3]])){
+//      return 1;
+//    }
+    return 0;
   }
 }
