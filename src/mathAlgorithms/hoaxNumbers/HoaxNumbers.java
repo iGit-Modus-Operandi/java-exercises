7c6c04bed4f9f21ff9920e355125d077bb114d5f
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-01-14 23:01:15 +0800

    [EXER]: Add initial code for finding hoax numbers

--- src/mathAlgorithms/hoaxNumbers/HoaxNumbers.java
+++ src/mathAlgorithms/hoaxNumbers/HoaxNumbers.java
@@ -2,4 +2,16 @@ package mathAlgorithms.hoaxNumbers;
 
 public class HoaxNumbers {
 
+  public static void main(String[] args) {
+    int n = 84;
+    if (isHoax(n)){
+      System.out.println("A Hoax number\n");
+    } else {
+      System.out.println("Not a Hoax number\n");
+    }
+    
+  }
+
+  private static boolean isHoax(int n) {
+  }
 }
