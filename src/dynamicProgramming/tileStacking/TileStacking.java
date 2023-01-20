f663f09f72ce93ce2cc9ed117a05044675563116
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-01-20 22:22:26 +0800

    [EXER]: Add initial code for solving the tile stacking problem

---
+++ src/dynamicProgramming/tileStacking/TileStacking.java
@@ -0,0 +1,12 @@
+package dynamicProgramming.tileStacking;
+
+public class TileStacking {
+
+  public static void main(String[] args) {
+    int n = 3, m = 3, k = 2;
+    System.out.println(possibleWays(n, m, k));
+  }
+
+  private static boolean possibleWays(int n, int m, int k) {
+  }
+}
