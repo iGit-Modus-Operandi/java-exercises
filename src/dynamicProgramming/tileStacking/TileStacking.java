6b7c007fcd061fde9e187a7c93236ca038b06a6a
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-01-23 23:32:51 +0800

    [EXER]: Update code for solving the tile stacking problem

--- src/dynamicProgramming/tileStacking/TileStacking.java
+++ src/dynamicProgramming/tileStacking/TileStacking.java
@@ -9,7 +9,7 @@ public class TileStacking {
     System.out.println(possibleWays(n, m, k));
   }
 
-  private static boolean possibleWays(int n, int m, int k) {
+  private static int possibleWays(int n, int m, int k) {
     int[][] dp = new int[N][N];
     int[][] presum = new int[N][N];
 
@@ -28,5 +28,20 @@ public class TileStacking {
     for (int i = 0; i < m + 1; i++) {
       presum[i][0] = dp[i][0] = 1;
     }
+
+    for (int i = 1; i < m + 1; i++) {
+      for (int j = 1; j < n + 1; j++) {
+        dp[i][j] = presum[i - 1][j];
+        if (j > k){
+          dp[i][j] -= presum[i - 1][j - k - 1];
+        }
+      }
+
+      for (int j = 1; j < n + 1; j++) {
+        presum[i][j] = dp[i][j] + presum[i][j - 1];
+      }
+    }
+
+    return dp[m][n];
   }
 }
