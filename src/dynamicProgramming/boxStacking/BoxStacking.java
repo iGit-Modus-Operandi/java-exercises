c606ed39b39a053697733ee2049a1fe8e0cde696
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-01-26 23:23:12 +0800

    [EXER]: Add initial conditions for solving the box stacking problem

--- src/dynamicProgramming/boxStacking/BoxStacking.java
+++ src/dynamicProgramming/boxStacking/BoxStacking.java
@@ -19,5 +19,14 @@ public class BoxStacking {
 
   public static void main(String[] args) {
     Box[] arr = new Box[4];
+    arr[0] = new Box(4, 6, 7);
+    arr[1] = new Box(1, 2, 3);
+    arr[2] = new Box(4, 5, 6);
+    arr[3] = new Box(10, 12, 32);
+
+    System.out.println("The maximum possible height of the stack is " + maxStackHeight(arr, 4));
+  }
+
+  private static String maxStackHeight(Box[] arr, int i) {
   }
 }
