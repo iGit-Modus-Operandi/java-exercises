dd627a2f15b6f836cd8360536e619e5b3fda1833
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-02-05 21:27:47 +0800

    [EXER]: Add initial code for solving the Knight's Tour problem

---
+++ src/backtrackingAlgorithms/knightsTour/KnightsTour.java
@@ -0,0 +1,13 @@
+package backtrackingAlgorithms.knightsTour;
+
+public class KnightsTour {
+
+  private static int N = 8;
+
+  public static void main(String[] args) {
+    solveKnightsTour();
+  }
+
+  private static void solveKnightsTour() {
+  }
+}
---
+++ src/backtrackingAlgorithms/test.java
@@ -0,0 +1,5 @@
+package backtrackingAlgorithms;
+
+public class test {
+
+}
