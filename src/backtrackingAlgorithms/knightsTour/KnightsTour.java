3b6d54333560fd6fd2d1e0757a4d9ec118115c2e
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-02-06 21:48:34 +0800

    [EXER]: Add code for the method of solving the Knight's tour problem

--- src/backtrackingAlgorithms/knightsTour/KnightsTour.java
+++ src/backtrackingAlgorithms/knightsTour/KnightsTour.java
@@ -8,6 +8,32 @@ public class KnightsTour {
     solveKnightsTour();
   }
 
-  private static void solveKnightsTour() {
+  private static boolean solveKnightsTour() {
+    int[][] sol = new int[8][8];
+
+    for (int x = 0; x < N; x++) {
+      for (int y = 0; y < N; y++) {
+        sol[x][y] = -1;
+      }
+    }
+
+    int[] xMove = {2, 1, -1, -2, -2, -1, 1, 2};
+    int[] yMove = {1, 2, 2, 1, -1, -2, -2, -1};
+
+    sol[0][0] = 0;
+
+    if(!solveKTUtil(0, 0, 1, sol, xMove, yMove)){
+      System.out.println("Solution does not exist");
+      return false;
+    } else {
+      printSolution(sol);
+    }
+    return true;
+  }
+
+  private static boolean solveKTUtil(int i, int i1, int i2, int[][] sol, int[] xMove, int[] yMove) {
+  }
+
+  private static void printSolution(int[][] sol) {
   }
 }
