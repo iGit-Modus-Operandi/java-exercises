1c1ef81beb736aed7824969b1b6ff6dfdd782800
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-02-20 21:11:26 +0800

    [EXER]: Add initial code for the n queen problem

---
+++ src/backtrackingAlgorithms/nQueen/NQueen.java
@@ -0,0 +1,15 @@
+package backtrackingAlgorithms.nQueen;
+
+public class NQueen {
+
+  public static void main(String[] args) {
+    NQueen Queen = new NQueen();
+    Queen.solveNQ();
+  }
+
+  private boolean solveNQ() {
+
+  }
+
+
+}
