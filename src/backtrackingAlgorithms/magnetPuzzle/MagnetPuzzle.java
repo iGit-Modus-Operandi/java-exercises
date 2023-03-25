bfbc159529cbf344aa2437be902eb296c5970fec
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-03-25 23:41:16 +0800

    [EXER]: Add initial codes for the magnet puzzle problem

---
+++ src/backtrackingAlgorithms/magnetPuzzle/MagnetPuzzle.java
@@ -0,0 +1,12 @@
+package backtrackingAlgorithms.magnetPuzzle;
+
+public class MagnetPuzzle {
+
+  public static void main(String[] args) {
+    char[][] rules = {};
+    solveMagnets(rules, 0, 0);
+  }
+
+  private static void solveMagnets(char[][] rules, int i, int i1) {
+  }
+}
