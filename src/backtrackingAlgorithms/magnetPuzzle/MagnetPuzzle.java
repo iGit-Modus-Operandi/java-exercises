fd6b16feab7f0bc1da30ee2eaabded8793e423d2
iGit-Modus-Operandi <monmon.nietes.acads@gmail.com>
2023-03-27 23:25:15 +0800

    [EXER]: Add inputs for array

--- src/backtrackingAlgorithms/magnetPuzzle/MagnetPuzzle.java
+++ src/backtrackingAlgorithms/magnetPuzzle/MagnetPuzzle.java
@@ -3,7 +3,13 @@ package backtrackingAlgorithms.magnetPuzzle;
 public class MagnetPuzzle {
 
   public static void main(String[] args) {
-    char[][] rules = {};
+    char[][] rules = {
+        {'L', 'R', 'L', 'R', 'T', 'T'},
+        {'L', 'R', 'L', 'R', 'B', 'B'},
+        {'T', 'T', 'T', 'T', 'L', 'R'},
+        {'B', 'B', 'B', 'B', 'T', 'T'},
+        {'L', 'R', 'L', 'R', 'B', 'B'}
+    };
     solveMagnets(rules, 0, 0);
   }
 
